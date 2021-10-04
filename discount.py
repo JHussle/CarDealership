710de814a1ea9804161e6a001012734e4ec57170
JSmith <65369149+JHussle@users.noreply.github.com>
2021-10-04 10:45:22 -0500

    logic placeholder

--- discount.py
+++ discount.py
@@ -1,1 +1,20 @@
 #discount on cars after purchase.
+amount = int(input("Enter Sales Amount: "))
+
+# checking comnditions and calculating discount
+# this is temp logic, placeholder
+
+if(amount>0):
+    if amount<= 5000:
+        discount = amount*0.25
+    elif amount<=15000:
+        discount = amount*0.30
+    elif amount<=25000:
+        discount=0.2 * amount
+    else:
+        discount=0.3 * amount
+
+    print("Discount : ", discount)
+    print("Net Pay : ", amount-discount)
+else:
+    print("Invalid Amount")
