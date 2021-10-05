84c41f8dfc20f5b367720f6f33fae8d1c31cee15
JSmith <65369149+JHussle@users.noreply.github.com>
2021-10-05 08:25:10 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,9 +2,9 @@ import mysql.connector, os
 from mysql.connector import connect 
 def returnConnection():
     conn = connect(
-        host = "mydatabasee.cphw5qnsj00k.us-east-1.rds.amazonaws.com",
-        user = "admin",
-        password = "rootroot",
-        database = "mydatabaseaci"
+        host = "",
+        user = "",
+        password = "",
+        database = ""
     )
     return conn
